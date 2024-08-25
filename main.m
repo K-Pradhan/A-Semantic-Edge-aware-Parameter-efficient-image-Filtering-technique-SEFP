95709fe096fa10840c0f2bce61d55c167bb42675
Kunal Pradhan <112946933+K-Pradhan@users.noreply.github.com>
2024-08-25 21:54:25 +0530

    Create main.m

---
+++ main.m
@@ -0,0 +1,18 @@
+% clear; clc; close all;
+%% Parameter input
+Widx = 1; % Optins- Widx = 1/2/3/4 
+
+%% Read input image 
+[InputImage] = imread('InputImages/cartoon.jpg');
+figure;
+imshow(InputImage);
+% InputImage = imnoise(InputImage,'gaussian',0,0.05);
+InputImage = im2double(InputImage);
+
+
+%% Call the SGI generation
+[FilteredImage] = EdgeMapGeneration(InputImage,Widx); 
+
+%% Show output filtered image
+figure;
+imshow(FilteredImage);
